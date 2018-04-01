a352e84058340a089f9d376923657899c2923387
Xavier Lin <xavier@emq.com>
2018-04-01 23:23:55 +0800

    add search function

--- app/src/main/java/com/xavier/newsfeed/model/NewsResponse.kt
+++ app/src/main/java/com/xavier/newsfeed/model/NewsResponse.kt
@@ -5,7 +5,6 @@ import com.xavier.newsfeed.newsFeedService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import java.io.Serializable
-import java.util.*
 
 /**
  * Created by xavier on 2018/3/31.
@@ -13,7 +12,28 @@ import java.util.*
 
 data class NewsItem(var title: String = "", var description: String = "", var urlToImage: String = "", var publishedAt: String = "") : Serializable
 
-data class NewsResponse(var nextPage: String = "", var news: MutableList<NewsItem> = ArrayList()) : Serializable {
+data class NewsResponse(var nextPage: String = "", var news: MutableList<NewsItem> = ArrayList(), var displayNews: MutableList<NewsItem> = news) : Serializable {
+
+  var filterString: String = ""
+
+  fun filterNews(){
+    displayNews = when (filterString.isEmpty()) {
+      true -> news
+      else -> news.filter {
+        it.title.contains(filterString, true) && it.description.contains(filterString, true)
+      }.toMutableList()
+    }
+  }
+
+  fun filterNewsIfChanged(filter: String): Boolean{
+    return if (filter != (filterString)) {
+        filterString = filter
+        filterNews()
+        true
+      }
+      else false
+  }
+
   companion object {
     fun getNews(url: String, successCallback: (NewsResponse) -> Unit, errorCallback: (Throwable) -> Unit) {
       disposable = newsFeedService.getNewsFeed(url).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
